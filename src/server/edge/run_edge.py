fd7cc12d443ee8c275a148989eadb0a46112dbea
Jun Wu Wang <wng.mattia@gmail.com>
2025-03-04 22:54:12 +0100

    Update run_edge.py

--- src/server/edge/run_edge.py
+++ src/server/edge/run_edge.py
@@ -3,22 +3,52 @@ from server.logger.log import logger
 from server.mqtt_client.mqtt_client import MqttClient
 from server.mqtt_client.mqtt_configs import MqttClientConfig
 
+import yaml
+from server.communication.websocket_server import WebsocketServer
+from server.communication.http_server import HttpServer
+from server.communication.request_handler import RequestHandler
+
+from server.commons import ConfigurationFiles
+
 if __name__ == "__main__":
     logger.info("Starting the [EDGE] MQTT client")
 
     # initialize edge inference times
     Edge.initialization()
 
-    # start the MQTT client
-    mqtt_client = MqttClient(
-        broker_url=MqttClientConfig.broker_url,
-        broker_port=MqttClientConfig.broker_port,
-        client_id=MqttClientConfig.client_id,
-        protocol=MqttClientConfig.protocol,
-        subscribed_topics=MqttClientConfig.subscribe_topics
-    )
+    with open(ConfigurationFiles.server_configuration_file_path, "r") as f:
+        config = yaml.safe_load(f)
+    
+    if 'websocket' in config['communication']['mode']:
+        websocket_config = config['communication']['websocket']
+        websocket_server = WebsocketServer(
+            host=websocket_config['host'],
+            port=websocket_config['port'],
+            endpoints=websocket_config['endpoints'],
+            ntp_server=websocket_config['ntp_server'],
+            last_offloading_layer=websocket_config['last_offloading_layer'],
+            request_handler=RequestHandler()
+        )
+        websocket_server.run()
 
-    # run the MQTT client in loop
-    mqtt_client.run()
+    if 'http' in config['communication']['mode']:
+        http_config = config['communication']['http']
+        http_server = HttpServer(
+            host=http_config['host'],
+            port=http_config['port'],
+            endpoints=http_config['endpoints'],
+            ntp_server=http_config['ntp_server'],
+            last_offloading_layer=http_config['last_offloading_layer'],
+            request_handler=RequestHandler()
+        )
+        http_server.run()
 
-    logger.info("Listening for messages...")
+    if 'mqtt' in config['communication']['mode']:
+        mqtt_client = MqttClient(
+            broker_url=MqttClientConfig.broker_url,
+            broker_port=MqttClientConfig.broker_port,
+            client_id=MqttClientConfig.client_id,
+            protocol=MqttClientConfig.protocol,
+            subscribed_topics=MqttClientConfig.subscribe_topics
+        )
+        mqtt_client.run()
